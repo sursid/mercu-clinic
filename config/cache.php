d08839dec2d03eff1d6a2de1bbb4dfb506989c92
SURS ID <sursid@ymgamestore.com>
2025-07-28 00:54:00 +0700

    ajsdhakhdajhd

--- config/cache.php
+++ config/cache.php
@@ -48,8 +48,8 @@ return [
 
         'file' => [
             'driver' => 'file',
-            'path' => storage_path('framework/cache/data'),
-            'lock_path' => storage_path('framework/cache/data'),
+            'path' => env('CACHE_PATH', '/tmp/storage/framework/cache/data'),
+            'lock_path' => env('CACHE_PATH', '/tmp/storage/framework/cache/data'),
         ],
 
         'memcached' => [
