1aeb5aa1d3831e0727b751c6efbb7fe740fd4314
SURS ID <sursid@ymgamestore.com>
2025-07-27 23:54:22 +0700

    Fix Vite config: Remove ESM-only dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+    plugins: [],
     build: {
         outDir: 'public/build',
         rollupOptions: {
+            input: {
+                app: 'resources/js/app.js',
+                css: 'resources/css/app.css'
+            },
             output: {
                 assetFileNames: 'assets/[name].[hash][extname]',
                 chunkFileNames: 'assets/[name].[hash].js',
